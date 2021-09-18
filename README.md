b0a8320ba443c2309c992d1056038a769a2b86f6
SpicyCombo <63688135+SpicyCombo@users.noreply.github.com>
2021-09-17 19:04:08 -0700

    test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# basicsurvival-xyz-cdn
-my own cdn shit
+#SpicyCombo's Website
+## Introduction
+Hi, welcome to the repository for my poorly written website in PHP. Yes I know,
+ it might just doesn't look that good... you can go to my website at 
+https://basicsurvival.xyz
+##Licenses
+I used some code from different websites, and I have listed them in the LICENSE.txt file.
+ I also have included my own license, which is LICENSE.txt. Generally, you are free to host
+ the website and modify the source code. But you must include my license and the license of 
+other accredited sources so their and my work would be accredited.
+## FAQ
+### Why did you use BasicSurvival.XYZ for your site's domain name?
+I bought that domain with NameCheap and it was used
+ for an Minecraft factions server, and I didn't want to spend much money. I could
+have bought another domain like SpicyCombo.com or something but I thought I'd just
+keep it.
---
+++ things/config.php
@@ -0,0 +1,25 @@
+<?php
+# CLIENT ID
+# https://i.imgur.com/GHI2ts5.png (screenshot)
+$client_id = "732395408160063520";
+
+# CLIENT SECRET
+# https://i.imgur.com/r5dYANR.png (screenshot)
+$secret_id = "fsHjtA-s41eqnuo-wdOx9XrbUwv_-1aZ";
+
+# SCOPES SEPARATED BY SPACE
+# example: identify email guilds connections  
+$scopes = "identify guilds.join";
+
+# REDIRECT URL
+# example: https://mydomain.com/includes/login.php
+# example: https://mydomain.com/test/includes/login.php
+$redirect_url = "https://xmc-classicube-site.herokuapp.com/includes/login.php";
+
+# IMPORTANT READ THIS:
+# - Set the `$bot_token` to your bot token if you want to use guilds.join scope to add a member to your server
+# - Check login.php for more detailed info on this.
+# - Leave it as it is if you do not want to use 'guilds.join' scope.
+
+# https://i.imgur.com/2tlOI4t.png (screenshot)
+$bot_token = "NzMyMzk1NDA4MTYwMDYzNTIw.Xwz-YA.DEofWxfuY0pEIn8R7FjppISqo6A";
